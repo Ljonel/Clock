a3a821e1f29889686bc45d487135786b0e67f30c
Damian <cygan.kontakt@gmail.com>
2019-06-21 00:32:02 +0200

    final

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,12 +8,12 @@ const color = document.querySelector(".actual-color");
 
 function showTime(){
     let today = new Date();
-    let hour = today.getHours();
+    let hour = today.getHours().toString();
     let min = today.getMinutes().toString();
     let sec = today.getSeconds().toString();
     
     if(hour.length < 2){
-        hour = "0"+hour;
+        hour = "0" + hour;
     }
     if(min.length < 2){
         min = "0" + min;
@@ -23,26 +23,27 @@ function showTime(){
     }
 
     //AM/PM
-   
-    if(hour < 12){
-        time.innerHTML = hour+'<span>:</span>'+min+'<span>:</span>'+sec+'AM';
-        document.body.style.backgroundImage="url('https://ljonel.github.io/Clock/img/afternoon.jpg')";
-        hello.textContent = "Good Morning";
-
-    }else if(hour < 18) {
-        time.innerHTML = hour+'<span>:</span>'+min+'<span>:</span>'+sec+'<span>  PM</span>';
-        document.body.style.backgroundImage="url('https://ljonel.github.io/Clock/img/mid.jpg')";
-        hello.textContent = "Good Afternoon";
-
-    }
-    else{
+    if(hour<6){
         time.innerHTML = hour+'<span>:</span>'+min+'<span>:</span>'+sec;
         document.body.style.backgroundImage="url('https://ljonel.github.io/Clock/img/night.png')"; 
         hello.textContent = "Good Evening";
         const quest = document.querySelector("#quest");
         quest.textContent = "What is your focus at this time?"
-
-
+    }
+    else if(hour < 12){
+        time.innerHTML = hour+'<span>:</span>'+min+'<span>:</span>'+sec+'AM';
+        document.body.style.backgroundImage="url('https://ljonel.github.io/Clock/img/afternoon.jpg')";
+        hello.textContent = "Good Morning";
+    }else if(hour < 18) {
+        time.innerHTML = hour+'<span>:</span>'+min+'<span>:</span>'+sec+'<span>  PM</span>';
+        document.body.style.backgroundImage="url('https://ljonel.github.io/Clock/img/mid.jpg')";
+        hello.textContent = "Good Afternoon";
+    }else{
+        time.innerHTML = hour+'<span>:</span>'+min+'<span>:</span>'+sec;
+        document.body.style.backgroundImage="url('https://ljonel.github.io/Clock/img/night.png')"; 
+        hello.textContent = "Good Evening";
+        const quest = document.querySelector("#quest");
+        quest.textContent = "What is your focus at this time?"
     }
     //set color
 
@@ -54,11 +55,24 @@ function showTime(){
 //clock color
 function setColor(){
     let today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
-    const write = "#"+hour+min+sec;
-    
+    let hour = today.getHours().toString();
+    let min = today.getMinutes().toString();
+    let sec = today.getSeconds().toString();
+    let write = "#"+hour+min+sec;
+
+    if(hour.length < 2){
+        hour = "0"+hour;
+        write = "#"+hour+min+sec;
+
+    }
+    if(min.length < 2){
+        min = "0" + min;
+        write = "#"+hour+min+sec;
+    }
+    if(sec.length < 2){
+        sec = "0"+sec;
+        write = "#"+hour+min+sec;
+    }
     //write color
     color.textContent = write;
     time.style.color="#"+hour+min+sec;
